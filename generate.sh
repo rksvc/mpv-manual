3055c877c3d8d049b6aa294de37724718f3a8120
rksvc <rksvc@qq.com>
2026-07-04 18:47:51 +0800

    use submodules

--- generate.sh
+++ generate.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-if [ ! -e mpv ]; then
-    git clone --depth 1 -b release/0.41 https://github.com/mpv-player/mpv
-fi
 find docs -name '*.md' -type f -delete
 pandoc mpv/DOCS/man/mpv.rst --lua-filter filter.lua
